220c34bd42d7efc96523866a5da5de61a165b80e
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-02 19:05:09 +0600

    added chefs data

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const cors = require("cors");
+app.use(cors());
+
+const port = process.env.Port || 5000;
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+// loading all chefs
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
