3ef5cd5daeba578e995608701fc74f13d062a767
Foysal Ahmmed <ifoysalahmmed@gmail.com>
2023-05-04 02:14:58 +0600

    updated server side

--- index.js
+++ index.js
@@ -5,23 +5,10 @@ app.use(cors());
 
 const port = process.env.Port || 5000;
 
-const categories = require("./data/categories.json");
 const chefs = require("./data/chefs.json");
 
 app.get("/", (req, res) => {
   res.send("hello world");
-});
-
-// loading all categories
-app.get("/categories", (req, res) => {
-  res.send(categories);
-});
-
-// loading single category
-app.get("/categories/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const selectedCategory = chefs.filter((c) => parseInt(c.category_id) === id);
-  res.send(selectedCategory);
 });
 
 // loading all chefs
